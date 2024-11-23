1d5545947069eca0d6993602d482e0d080677bdb
Bruno13f <122650083+Bruno13f@users.noreply.github.com>
2024-11-23 00:18:29 +0000

    DB & pdf model 🦭🦭
    - remover dependecias desnecessárias

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -26,12 +26,18 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'role' => fake()->randomElement(['S', 'T']),
         ];
     }
 
+    public function withRole(string $role): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => $role,
+        ]);
+    }
+    
     /**
      * Indicate that the model's email address should be unverified.
      */
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Pdf;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +17,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Fernando Laravel',
+            'email' => 'flav@pt.pt',
+            'role' => 'S'
         ]);
+        User::factory()->create([
+            'name' => 'Margarida Fofinha',
+            'email' => 'magfofa@pt.pt',
+            'role' => 'T'
+        ]);
+        Pdf::factory()->create();
     }
 }
