a163f2a0fe7c49a47aec0d9f3dd323a935d96c48
Bruno13f <122650083+Bruno13f@users.noreply.github.com>
2024-11-23 00:58:14 +0000

    chore: change template 🍾🥂

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,12 +11,9 @@ import {
 } from '@/components/ui/card'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
-import { CalendarDateRangePicker } from '@/components/date-range-picker'
 import { MainNav } from '@/components/main-nav'
 import { Overview } from '@/components/overview'
 import { RecentSales } from '@/components/recent-sales'
-import { Search } from '@/components/search'
-import TeamSwitcher from '@/components/team-switcher'
 import { UserNav } from '@/components/user-nav'
 import { ThemeToggle } from '@/components/theme-toggle'
 
@@ -47,37 +44,22 @@ export default function DashboardPage() {
       <div className='hidden flex-col md:flex'>
         <div className='border-b'>
           <div className='flex h-16 items-center px-4'>
-            <TeamSwitcher />
             <MainNav className='mx-6' />
             <div className='ml-auto flex items-center space-x-4'>
-              <Search />
               <ThemeToggle />
               <UserNav />
             </div>
           </div>
         </div>
         <div className='flex-1 space-y-4 p-8 pt-6'>
-          <div className='flex items-center justify-between space-y-2'>
-            <h2 className='text-3xl font-bold tracking-tight'>Dashboard</h2>
-            <div className='flex items-center space-x-2'>
-              <CalendarDateRangePicker />
-              <Button>Download</Button>
-            </div>
-          </div>
-          <Tabs defaultValue='overview' className='space-y-4'>
+          <Tabs defaultValue='subjects' className='space-y-4'>
             <TabsList>
-              <TabsTrigger value='overview'>Overview</TabsTrigger>
-              <TabsTrigger value='analytics' disabled>
-                Analytics
-              </TabsTrigger>
-              <TabsTrigger value='reports' disabled>
-                Reports
-              </TabsTrigger>
-              <TabsTrigger value='notifications' disabled>
-                Notifications
+              <TabsTrigger value='subjects'>Subjects</TabsTrigger>
+              <TabsTrigger value='history' disabled>
+                History
               </TabsTrigger>
             </TabsList>
-            <TabsContent value='overview' className='space-y-4'>
+            <TabsContent value='subjects' className='space-y-4'>
               <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4'>
                 <Card>
                   <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
--- frontend/components/main-nav.tsx
+++ frontend/components/main-nav.tsx
@@ -13,27 +13,9 @@ export function MainNav({
     >
       <Link
         href='/examples/dashboard'
-        className='text-sm font-medium transition-colors hover:text-primary'
+        className='text-3xl font-bold tracking-tight'
       >
-        Overview
-      </Link>
-      <Link
-        href='/examples/dashboard'
-        className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'
-      >
-        Customers
-      </Link>
-      <Link
-        href='/examples/dashboard'
-        className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'
-      >
-        Products
-      </Link>
-      <Link
-        href='/examples/dashboard'
-        className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'
-      >
-        Settings
+        SubjectsMate
       </Link>
     </nav>
   )
