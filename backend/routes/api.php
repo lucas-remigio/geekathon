dd31f71fdd2711336e0d8128fa2d0fd5f9d80500
Bruno13f <122650083+Bruno13f@users.noreply.github.com>
2024-11-23 23:17:40 +0000

    feat: endpoint to get all subjects and chapters from subject

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,12 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\AwsController;
 use App\Http\Controllers\api\PdfController;
+use App\Http\Controllers\api\SubjectController;
 
 
 Route::post('/submit-mcq-results', [AwsController::class, 'correctMultipleChoiceQuestions']);
 Route::post('/pdfs', [PdfController::class, 'upload']);
 Route::get('/pdfs', [PdfController::class, 'index']);
 Route::get('/pdfs/{id}', [PdfController::class, 'show']);
+Route::get('/subjects', [SubjectController::class, 'index']);
+Route::get('/subjects/{id}/chapters', [SubjectController::class, 'getChapters']);
 Route::post('/generate-test', [AwsController::class, 'generateTest']);
 Route::post('/correct-answer', [AwsController::class, 'correctExtensiveQuestion']);
 Route::post('/generate-summaries', [AwsController::class, 'generateSummaries']);
